e29fd7d12bf4b9afbb42488889ba534032b80181
camilito2005 <marrugobarrioscamilo2005@gmail.com>
2025-03-18 19:58:26 -0500

    feat: agregue las respectivas rutas para realizar el registro de los usuarios

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UsuariosController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+Route::get('/usuarios', [UsuariosController::class,'Formulario'])->name('usuarios');
+Route::post('/usuarios/Registrar', [UsuariosController::class,'RegistrarU'])->name('usuarios.registrar');
