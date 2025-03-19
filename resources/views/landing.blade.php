c63d4febe7a4ff683b21a2e6b0757d3bc453a839
camilito2005 <marrugobarrioscamilo2005@gmail.com>
2025-03-19 17:35:10 -0500

    feat: agregue un boton para que me rediriga al inicio

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Droguería SaludPlus</title>
+    <title>...</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 </head>
 <body>
@@ -29,6 +29,7 @@
                     <li class="nav-item"><a class="nav-link" href="#servicios">Servicios</a></li>
                     <li class="nav-item"><a class="nav-link" href="#productos">Productos</a></li>
                     <li class="nav-item"><a class="nav-link" href="#contacto">Contacto</a></li>
+                    <li class="nav-item"><a class="nav-link" href="{{route('index')}}">Inicio</a></li>
                 </ul>
             </div>
         </div>
