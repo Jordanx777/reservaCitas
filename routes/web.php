801aa9baf979c332f19b7e2fcbdd490ea25bddcd
camilito2005 <marrugobarrioscamilo2005@gmail.com>
2025-03-19 17:03:25 -0500

    feat: añadi nuevas rutas y documente

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,17 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UsuariosController;
 
-Route::get('/', function () {
-    return view('index');
-});
-Route::get('/usuarios', [UsuariosController::class,'Formulario'])->name('usuarios');
+// esta ruta es la que se ejecuta al iniciar el proyecto
+Route::get('/', [UsuariosController::class,'Index'])->name('index');
+// esta ruta es para el formulario de registro
+Route::get('/usuarios', [UsuariosController::class,'Formulario'])->name('usuarios.formulario');
+//esta es para registrar los datos del formulario
 Route::post('/usuarios/Registrar', [UsuariosController::class,'RegistrarU'])->name('usuarios.registrar');
+// esta es para la vista de la landing la cual uso cuando inicia sesion
+Route::get('/landing', [UsuariosController::class,'Landing'])->name('landing');
+// esta es para la vista del login
+Route::get('/Login', [UsuariosController::class,'Login_html'])->name('login_html');
+// esta es para iniciar la sesion
+Route::post('/Login/inicio', [UsuariosController::class,'Login'])->name('login');
+// esta es para cerrar la sesion
+Route::get('/Login/Cerrar', [UsuariosController::class,'Cerrar'])->name('Cerrar');
