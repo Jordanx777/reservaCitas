0dac302f7377400109adca88393f95d2d0225c97
camilito2005 <marrugobarrioscamilo2005@gmail.com>
2025-03-19 17:09:39 -0500

    feat: añadi las funcionalidades del login y logout

--- app/Http/Controllers/UsuariosController.php
+++ app/Http/Controllers/UsuariosController.php
@@ -8,15 +8,23 @@ use Illuminate\Support\Facades\Hash;
 
 class UsuariosController extends Controller
 {
+    public function Index(){
+        return view('index');
+    }
     public function Formulario()
     {
-        // return view('formulario');
         return view('usuarios.formulario');
-
+    }
+    public function Landing(){
+        return view('landing');
+    }
+    public function Login_html(){
+        return view('usuarios/login');
     }
     public function RegistrarU(Request $request)
     {
 
+        // verifica que los campos no esten vacios y en el campo de correo sea tipo correo y que la contraseña verifica que sea mayor a 6 caracteres y que la confirmacion de la contraseña sea igual a la contraseña
         $request->validate([
             'nombre' => 'required',
             'apellidos' => 'required',
@@ -26,6 +34,7 @@ class UsuariosController extends Controller
             'contraseña' => 'required|min:6|confirmed',
         ]);
 
+        //obtiene los valores de los campos
         $nombre = $request->input('nombre');
         $apellidos = $request->input('apellidos');
         $telefono = $request->input('telefono');
@@ -34,8 +43,9 @@ class UsuariosController extends Controller
         $contraseña = $request->input('contraseña');
         $confirmar_contraseña = $request->input('contraseña_confirmation');
 
-        dump("Nombre: $nombre, Apellidos: $apellidos, Telefono: $telefono, Edad: $edad, Correo: $correo Contraseña: $contraseña, Confirmar Contraseña: $confirmar_contraseña");
+        // dump("Nombre: $nombre, Apellidos: $apellidos, Telefono: $telefono, Edad: $edad, Correo: $correo Contraseña: $contraseña, Confirmar Contraseña: $confirmar_contraseña");
 
+        //inserta los valores en la base de datos
         $consulta = DB::table('Usuarios')->insert([
             'nombre' => $nombre,
             'apellidos' => $apellidos,
@@ -45,14 +55,64 @@ class UsuariosController extends Controller
             'contraseña' => Hash::make($contraseña),
             'created_at' => now(),
         ]);
-        dump($consulta);
+        // dump($consulta);
 
+        //si la consulta es correcta redirecciona al formulario con un mensaje
         if ($consulta) {
-            return redirect()->route('usuarios')->with('mensaje', 'Usuario registrado correctamente');
+            return redirect()->route('usuarios.formulario')->with('mensaje', 'Usuario registrado correctamente');
         } else {
-            return redirect()->route('usuarios')->with('error', 'algo a fallado correctamente');
+            //si la consulta es incorrecta redirecciona al formulario con un mensaje
+            return redirect()->route('usuarios.formulario')->with('error', 'algo a fallado correctamente');
         }
 
     }
+    public function Login(Request $request){
+        //verifica que los campos no esten vacios y en el campo de los correos sea tipo correo
+        $requerido = $request->validate([
+            'correo' => 'required|email',
+            'contraseña' => 'required',
+        ]);
+        // dump($requerido);
+
+        //obtiene los valores de los campos en este caso correo y contraseña
+        $correo = $request->input('correo');
+        $contraseña = $request->input('contraseña');
+
+        //consulta a la base de datos si el correo existe
+        $consulta = DB::table('Usuarios')->where('correo', $correo)->first();
+        // dump($consulta);
+
+        //si el correo existe
+        if($consulta){
+            //guarda los valores de la consulta en variables de sesion
+           session([
+                'nombre' => $consulta->nombre,
+                'apellidos' => $consulta->apellidos,
+                'telefono' => $consulta->telefono,
+                'edad' => $consulta->edad,
+                'correo' => $consulta->correo,
+                'created_at' => $consulta->created_at,
+            ]);
+            // verifica si la contraseña es correcta
+            if(Hash::check($contraseña, $consulta->contraseña)){
+                //si es correcta redirecciona a la vista landing
+                return redirect()->route('landing')->with(['mensaje'=> 'Bienvenido', 'color' => 'green']);
+                // si no es correcta redirecciona al login
+            }else{
+                return redirect()->route('login_html')->with(['mensaje'=> 'Contraseña incorrecta', 'color' => 'red']);
+            }
+        }else{
+            //si el correo no existe redirecciona al login
+            return redirect()->route('login_html')->with(['mensaje'=> 'Correo incorrecto', 'color' => 'red']);
+        }
+
+    }
+    public function Cerrar(){
+        //elimina todas las variables de sesion por ende la sesion
+        session()->flush();
+        // y redirecciona al login
+        return redirect()->route('login_html')->with(['mensaje'=> 'Sesion cerrada', 'color' => 'green']);
+    }
+    
     
 }
