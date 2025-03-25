be383865f53c52ef913e4996d9f87fc8d07c03ff
camilito2005 <marrugobarrioscamilo2005@gmail.com>
2025-03-25 17:02:47 -0500

    feat: cree el modelo para los usuarios

--- app/Models/UsuarioModel.php
+++ app/Models/UsuarioModel.php
@@ -3,8 +3,24 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Cargo;
 
 class UsuarioModel extends Model
 {
-    //
+    public $timestamps = false;
+    use HasFactory;
+
+    protected $table = 'Usuarios';
+
+    protected $fillable = [
+        'nombre', 'apellidos', 'telefono', 
+        'edad', 'correo', 'contraseña', 
+        'cargo_id', 'created_at', 'updated_at'
+    ];
+
+    protected $hidden = ['contraseña'];
+    public function cargo() {
+        return $this->belongsTo(Cargo::class, 'cargo_id', 'id'); 
+    }
 }
