53261a320feba1c5b7db80344d80ae669fece4fa
Luis H <luisinfotv@gmail.com>
2025-03-26 13:24:17 -0500

    #

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,17 +1,37 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel</title>
 </head>
-<body >
+
+<body>
     <h3>hola</h3>
     <a href="{{ route('usuarios') }}">formulario</a>
-<form action="{{ route('usuarios') }}" method="GET">
-    @csrf
-    <input type="submit" value="formulario">
-</form>
+    <form action="{{ route('usuarios') }}" method="GET">
+        @csrf
+        <input type="submit" value="formularioq">
+        <script>
+            const Toast = Swal.mixin({
+                toast: true,
+                position: "top-end",
+                showConfirmButton: false,
+                timer: 4000,
+                timerProgressBar: true,
+                didOpen: (toast) => {
+                    toast.onmouseenter = Swal.stopTimer;
+                    toast.onmouseleave = Swal.resumeTimer;
+                }
+            });
+            Toast.fire({
+                icon: "success",
+                title: "Signed in successfully"
+            });
+        </script>
+    </form>
 </body>
+
 </html>
