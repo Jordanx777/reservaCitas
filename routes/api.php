5ac8c93f0e9a98e4ea64241ca78921787f585adf
camilito2005 <marrugobarrioscamilo2005@gmail.com>
2025-03-29 14:40:34 -0500

    feat: cree el archivo para las rutas de las apis

---
+++ routes/api.php
@@ -0,0 +1,18 @@
+<?php
+use App\Http\Controllers\HorariosController;
+use Illuminate\Support\Facades\Route;
+
+// dd('El archivo api.php está siendo cargado correctamente');
+// use App\Http\Controllers\HorariosController;
+
+
+// Route::get('/horarios', [HorariosController::class, 'index']);
+// Route::post('/horarios', [HorariosController::class, 'store']);
+// Route::put('/horarios/reservar/{id}', [HorariosController::class, 'Reservar']);
+// Route::put('/horarios/cancelar/{id}', [HorariosController::class, 'Cancelar']);
+
+Route::get('/horarios', [HorariosController::class, 'index']);
+Route::post('/horarios', [HorariosController::class, 'store']);
+Route::put('/horarios/reservar/{id}', [HorariosController::class, 'Reservar']);
+Route::put('/horarios/cancelar/{id}', [HorariosController::class, 'Cancelar']);
+
