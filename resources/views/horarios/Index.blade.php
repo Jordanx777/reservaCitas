cffc20b8a06a23bb3b931551251024dd767262c9
camilito2005 <marrugobarrioscamilo2005@gmail.com>
2025-03-29 21:48:32 -0500

    feat: agregue el formulario para agregar los horarios

--- resources/views/horarios/Index.blade.php
+++ resources/views/horarios/Index.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.menu')
 @section('titulo', 'horarios')
 @section('contenido')
+@push('meta')
+<meta name="csrf-token" content="{{ csrf_token() }}"> 
+@endpush
 <div class="container">
     <h2 class="text-center">Lista de Horarios </h2>
     <table class="table">
@@ -18,6 +21,36 @@
         </tbody>
     </table>
 </div>
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalHorario">
+    Agregar Horario
+</button>
+<!-- Modal -->
+<div class="modal fade" id="modalHorario" tabindex="-1" aria-labelledby="modalHorarioLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="modalHorarioLabel">Agregar Nuevo Horario</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Cerrar"></button>
+        </div>
+        <div class="modal-body">
+          <form id="formHorario">
+            @csrf
+            <div class="mb-3">
+              <label for="fecha" class="form-label">Fecha:</label>
+              <input type="date" class="form-control" id="fecha" name="fecha" required>
+            </div>
+            <div class="mb-3">
+              <label for="hora" class="form-label">Hora:</label>
+              <input type="time" class="form-control" id="hora" name="hora" required>
+            </div>
+            <button type="submit" class="btn btn-success">Guardar</button>
+          </form>
+          <div id="mensaje" class="mt-3"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+  
 @push('scripts')
     <script src="{{asset('js/horarios.js')}}"></script>
 @endpush
