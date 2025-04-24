28da3b04315bbc9642d07d033b3882841b9852cb
camilito2005 <marrugobarrioscamilo2005@gmail.com>
2025-04-23 22:49:51 -0500

    feat: agregue las respectivas rutas para las apis

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,14 @@
 <?php
 use App\Http\Controllers\HorariosController;
+use App\Notifications\CitaConfirmada;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('/horarios', [HorariosController::class, 'index']); // ruta para mostrar el inicio
 Route::post('/horarios', [HorariosController::class, 'store']); // ruta para mostrar los horarios registrados 
 Route::put('/horarios/Editar/{id}',[HorariosController::class,'Editar']); // ruta para editar los horarios
-Route::put('/horarios/reservar/{id}', [HorariosController::class, 'Reservar']); // ruta para reservar una cita 
+Route::put('/horarios/reservar/{id}/{id_usuario}', [HorariosController::class, 'Reservar']); // ruta para reservar una cita 
 Route::put('/horarios/cancelar/{id}', [HorariosController::class, 'Cancelar']);// ruta para cancelar una cita 
 Route::delete('/horarios/Eliminar/{id}', [HorariosController::class, 'Eliminar']); // ruta para eliminar los horarios
+
 
